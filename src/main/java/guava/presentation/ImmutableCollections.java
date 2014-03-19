83ec646b60ce483e5592778f6bbbd3a058123c49
Juan Ignacio Vimberg <jivimberg@gmail.com>
2014-03-18 23:32:36 -0700

    Finished immutableCollections

--- src/main/java/guava/presentation/ImmutableCollections.java
+++ src/main/java/guava/presentation/ImmutableCollections.java
@@ -1,7 +1,58 @@
 package guava.presentation;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
- * Created by jivimberg on 3/12/14.
+ * Why immutable?
+ *  - Thread safe
+ *  - Memory and space efficient
+ *  - Can be treated as constants as they remain fixed
  */
+@SuppressWarnings("UnusedDeclaration")
 public class ImmutableCollections {
+
+    /**
+     * The JDK provides Collections.unmodifiableXXX methods
+     */
+    public Set<String> jdkUnmodifiable(){
+        Set<String> modifiableSet = new HashSet<String>();
+        modifiableSet.add("a");
+        modifiableSet.add("b");
+        modifiableSet.add("c");
+        return Collections.unmodifiableSet(modifiableSet); //Inefficient according to Guava Documentation
+    }
+
+    public Set<String> guavaUnmodifiable(){
+        return ImmutableSet.of("a", "b", "c");
+    }
+
+    /**
+     * the JDK collections are only truly immutable if nobody holds a reference to the original collection
+     */
+    public static void main(String[] args) {
+        HashSet<String> modifiableSet = Sets.newHashSet("a", "b", "c");
+
+        Set<String> jdkUnmodifiable = Collections.unmodifiableSet(modifiableSet);
+        ImmutableSet<String> guavaUnmodifiable = ImmutableSet.copyOf(modifiableSet);
+
+        modifiableSet.remove("a");
+        System.out.println(jdkUnmodifiable.size()); // prints 2
+        System.out.println(guavaUnmodifiable.size()); // prints 3
+    }
+
+    public ImmutableSet<String> usingBuilder(){
+        HashSet<String> modifiableSet = Sets.newHashSet("a", "b", "c");
+
+        return ImmutableSet.<String>builder()
+                        .addAll(modifiableSet)
+                        .add("d")
+                        .add("e", "f", "g")
+                        .build();
+    }
+
 }
